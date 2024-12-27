7b24f6abea6e583645d4bda07c9f01f260d8e491
Coir-hat-man <liuhm59@mail2.sysu.edu.cn>
2024-12-27 18:38:37 +0800

    nccl test

--- Gpipe/test.py
+++ Gpipe/test.py
@@ -1,16 +1,15 @@
 import torch
-import torch.optim as optim
-from torch.cuda.amp import autocast, GradScaler
 
-class Model(torch.nn.Module):
-    def __init__(self,layer):
-        super(Model, self).__init__()
-        self.fc = layer
+# device group setting
+dist.init_process_group(backend='nccl')
+world_size=dist.get_world_size()    # 程序使用的总进程数
+global_rank=dist.get_rank()
+local_size=torch.cuda.device_count()    # 当前节点上存在几张显卡
+local_rank=int(os.environ["LOCAL_RANK"]) # 进程在当前节点上的序号
+torch.cuda.set_device(local_rank%local_size) # 确保进程在多个GPU上是平分的
 
-    def my_try(self):
-        Init(layer=torch.nn.Linear(1, 1))
+data = torch.rand(16,512,4096).to("cuda") # seq_length, batch_size/num_chunks, hidden_size 512, 16, 4096
 
-model = Model(layer=torch.nn.Linear(1, 1))
-model.my_try()
+
 
 
